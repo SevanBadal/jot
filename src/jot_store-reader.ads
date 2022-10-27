41b100faf0ac53fcce6e5788ae4256bcb6203007
SevanBadal <sgolnaza@gmail.com>
2022-10-27 00:03:50 -0400

    implement single query procedure

--- src/jot_store-reader.ads
+++ src/jot_store-reader.ads
@@ -1,4 +1,7 @@
+with Ada.Directories; use Ada.Directories;
+
 package Jot_Store.Reader is
 	procedure List_Jots;
-	procedure Search_Jots;
+	procedure Search_Jots (Query : in String);
+	procedure Print_File (Dir : in Directory_Entry_Type);
 end Jot_Store.Reader;
