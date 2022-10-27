0386e52b79163c4a4b7986ddd819fe58245fee76
SevanBadal <sgolnaza@gmail.com>
2022-10-27 00:06:07 -0400

    remove returns and restructure if statement

--- src/jot.adb
+++ src/jot.adb
@@ -9,32 +9,30 @@ procedure Jot is
 begin
    if Ada.Command_Line.Argument_Count = 0 then
       Jot_Store.Reader.List_Jots;
-      return;
    elsif Ada.Command_Line.Argument_Count = 1 then
-      Jot_Store.Reader.Search_Jots;
-      return;
+      Jot_Store.Reader.Search_Jots (Query =>  Ada.Command_Line.Argument (1));
    elsif Ada.Command_Line.Argument_Count /= 3 then
       Ada.Text_IO.Put_Line ("Wrong number of arguments");
-      return;
+   else 
+      declare
+         Note_Title : constant String := Ada.Command_Line.Argument (1);
+         Note_Tags : constant String := Ada.Command_Line.Argument (2);
+         Note_Body : constant String := Ada.Command_Line.Argument (3);
+         Res : Boolean := True;
+         Jot : Jot_Store.Jot := (
+            Title_Length => Note_Title'Length,
+            Tag_Length => Note_Tags'Length,
+            Body_Length => Note_Body'Length,
+            Title => Note_Title,
+            Tags => Note_Tags,
+            Jot_Body => Note_Body
+         );
+      begin
+         Jot_Config.Configure_Datastore;
+         Ada.Text_IO.Put_Line ("Note Title: " & Jot.Title);
+         Ada.Text_IO.Put_Line ("Note Tags: " & Note_Tags);
+         Ada.Text_IO.Put_Line ("Note Body: " & Note_Body);
+         Jot_Store.Writer.Create_Jot (JotNote => Jot);
+      end;
    end if;
-   declare
-      Note_Title : constant String := Ada.Command_Line.Argument (1);
-      Note_Tags : constant String := Ada.Command_Line.Argument (2);
-      Note_Body : constant String := Ada.Command_Line.Argument (3);
-      Res : Boolean := True;
-      Jot : Jot_Store.Jot := (
-         Title_Length => Note_Title'Length,
-         Tag_Length => Note_Tags'Length,
-         Body_Length => Note_Body'Length,
-         Title => Note_Title,
-         Tags => Note_Tags,
-         Jot_Body => Note_Body
-      );
-   begin
-      Jot_Config.Configure_Datastore;
-      Ada.Text_IO.Put_Line ("Note Title: " & Jot.Title);
-      Ada.Text_IO.Put_Line ("Note Tags: " & Note_Tags);
-      Ada.Text_IO.Put_Line ("Note Body: " & Note_Body);
-      Res := Jot_Store.Writer.Create_Jot (JotNote => Jot);
-   end;
 end Jot;
