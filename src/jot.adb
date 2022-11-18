ff2e33fdd446e28531e880df8614e4dc0995cff7
SevanBadal <sgolnaza@gmail.com>
2022-11-18 12:43:53 -0500

    flag refactor init

--- src/jot.adb
+++ src/jot.adb
@@ -1,20 +1,37 @@
-with Ada.Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Command_Line;
-with Jot_Config;
+with Jot_Config; use Jot_Config;
 with Jot_Store;
 with Jot_Store.Writer;
 with Jot_Store.Reader;
 
 procedure Jot is
+   Search_Flags : Flag_Array :=
+      (Flag'First .. Flag'Last => False);
 begin
    if Ada.Command_Line.Argument_Count = 0 then
       Jot_Store.Reader.List_Jots;
    elsif Ada.Command_Line.Argument_Count = 1 then
       Jot_Store.Reader.Search_Jots (Query =>  Ada.Command_Line.Argument (1));
-   elsif Ada.Command_Line.Argument_Count = 2 then
-      if Ada.Command_Line.Argument (1) = "-b" then
-         Jot_Store.Reader.Search_Jot_Bodies (Ada.Command_Line.Argument (2));
+   elsif Ada.Command_Line.Argument_Count > 1 then
+      Jot_Store.Reader.Parse_Flags
+         (Flag_Strings => Ada.Command_Line.Argument (1),
+          Jot_Flags    => Search_Flags);
+      for Flag of Search_Flags loop
+         Put_Line (Flag'Image);
+      end loop;
+      if Search_Flags (B) then
+         Put_Line ("Body Search");
       end if;
+      if Search_Flags (P) then
+         Put_Line ("Path Result");
+      end if;
+      if Search_Flags (T) then
+         Put_Line ("Title and Tag Search");
+      end if;
+      -- if Ada.Command_Line.Argument (1) = "-b" then
+      --    Jot_Store.Reader.Search_Jot_Bodies (Ada.Command_Line.Argument (2));
+      -- end if;
    elsif Ada.Command_Line.Argument_Count /= 3 then
       Ada.Text_IO.Put_Line ("Wrong number of arguments");
    else
--- src/jot_store-reader.ads
+++ src/jot_store-reader.ads
@@ -1,9 +1,12 @@
 with Ada.Directories; use Ada.Directories;
-
+with Jot_Config; use Jot_Config;
 package Jot_Store.Reader is
    procedure List_Jots;
    procedure Search_Jots (Query : String);
    procedure Search_Jot_Bodies (Query : String);
    procedure Search_Jot_Bodies_Nested (TQuery : String; BQuery : String);
    procedure Print_File (Dir : Directory_Entry_Type);
+   procedure Parse_Flags (Flag_Strings : String; Jot_Flags : in out Flag_Array);
+private
+   Flag_Delimiter : constant Character := '-';
 end Jot_Store.Reader;
